69782526bade56b60c840603d28a305c12605b6f
johanp0308 <sebastian-johan1@hotmail.com>
2023-10-09 15:58:47 -0500

    feat: :sparkles: Creating a general Crud method
    This method seeks to generalize the requests and to further simplify the codes of the other relations of the json

---
+++ src/storages/crurd.js
@@ -0,0 +1,35 @@
+import {env} from "../config/config.js";
+
+
+/* Consts */
+const url = `${env.ssl+env.host}:${env.port}`;
+const config = {
+    method:undefined,
+    headers:{"content-type":"application/json"}
+}
+
+export const getAll = async (enpoint) => {
+    let res = await (await fetch(`${url}/${enpoint}`)).json();
+    return res;
+}
+export const getOne = async(endpoint,id) => {
+    let res = await (await fetch(`${url}/${endpoint}/${id}`)).json();
+    return res;
+}
+export const post = async (obj={},endpoint) => {
+    config.method = 'POST';
+    config.body = JSON.stringify(obj);
+    let res = await(await fetch(`${url}/${endpoint}`,)).json();
+    return res;
+}
+export const deleteOne = async (endpoint,id) => {
+    config.method = 'DELETE';
+    let res = await( await fetch(`${url}/${endpoint}/${id}`,config)).json();
+    return res;
+}
+export const putOne = async (obj = {},endpoint,id) => {
+    config.method = 'PUT';
+    config.body = JSON.stringify(obj);
+    let res = await(await fetch(`${url}/${endpoint}/${id}`,config)).json();
+    return res;
+}
