fd833c068f938b0e4c3014dc8783193bf46978c0
johanp0308 <sebastian-johan1@hotmail.com>
2023-10-09 17:18:24 -0500

    feat: :sparkles: Added a functional crud to users

--- src/storages/users.js
+++ src/storages/users.js
@@ -1,16 +1,57 @@
-import {env} from "../config/config.js";
-const enpoint = "users";
-const url = `${env.ssl+env.host}:${env.port}/${enpoint}`;
+import {
+    valiEstructUser as validation,
+    compareEstructure as compare
+} from '../util/validation.js'
 
-export const getAll = () => {
-}
-export const getOne = (id) => {
-}
-export const post = (obj = {}) => {
-}
-export const deleteOne = (id) => {
-}
-export const putOne = (obj = {}) => {
+import {
+    getAll,
+    getOne,
+    post,
+    deleteOne,
+    putOne
+} from './crurd.js'
+
+/* Consts */
+const enpointUser = 'users'
+
+export const getAllUsers = () =>{
+    return getAll(enpointUser);
 }
 
-console.log(env);
+export const getOneUser = (id) =>{
+    return getOne(enpointUser,id);
+}
+
+export const postUser = (obj={}) =>{
+    let objVal = validation(obj);
+    if(objVal.status) return objVal.message;
+    return post(objVal,enpointUser);
+}
+
+export const deleteOneUser = (id) =>{
+    return deleteOne(enpointUser,id)
+}
+
+export const putOneUser = async (obj,id) =>{
+    let objVal = validation(obj);
+    if(objVal.status) return objVal.message;
+    let newEdit = compare(await getOneUser(id),objVal);
+    return putOne(newEdit,enpointUser,id);
+}
+
+// let ser = {
+//     name:"Jaider",
+//     aboutme:"I am Papucho",
+//     contacts:[
+//         {"linkedin":"jdiaer.com"},
+//         {"email":"yadier@gmail.com"},
+//         {"phone":"798165"},
+//         {"github":"@jiader"}
+//     ],
+//     skillsId:[1,2],
+//     habilitiesId:[1,2],
+//     languagesId:[1,2],
+//     statusId:3
+// }
+
+// console.log(await putOneUser(ser,3))
