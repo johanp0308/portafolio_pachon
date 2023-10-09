009137f2fa043d52194f5d67f30d8f4be65ae0da
Johan Sebastian Pachon <sebastian-johan1@hotmail.com>
2023-10-09 13:24:07 -0500

    feat: :technologist: Add new changes

--- src/util/validation.js
+++ src/util/validation.js
@@ -1,5 +1,19 @@
 
 
-const valiEstruct = (obj) => {
+const valiEstructUser = (obj) => {
+    if(!(obj.constructor.name !== "Object" && Object.keys(obj)>0)) return {status:400,message:"This object is incorrect"};
+    let {name,aboutme,contacts,skillsId,habilitiesId,languages,statusId} = obj;
+    if(!(contacts.constructor.name !== "Object") && Object.keys(contacts)>0) return {status:400,message:`This ${contacts} is not incorrect`}
     
+    if(typeof name !== "string") return {status:400,message:`this attribute is incorrect: ${name}`};
+    if(typeof aboutme !== "string") return {status:400,message:`this attribute is incorrect: ${aboutme}`};
+    if(habilitiesId.constructor.name !== "Array") return {status:400,message:`this attribute is incorrect: ${name}`};
+    if(skillsId.constructor.name!== "string") return {status:400,message:`this attribute is incorrect: ${name}`};
+    if(typeof name === "string") return {status:400,message:`this attribute is incorrect: ${name}`};
+    
+}
+
+const valiEstructSkill = (obj) => {
+    if(!(obj.constructor.name !== "Object" && Object.keys(obj)>0)) return {status:404,message:"This object is incorrect"};
+
 }
