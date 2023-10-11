cc3c5cb03894a992a4e783751d2449d46be33546
johanp0308 <sebastian-johan1@hotmail.com>
2023-10-10 23:35:15 -0500

    feat: :sparkles: Defining general listeners

---
+++ app.js
@@ -0,0 +1,15 @@
+/* Imports */
+// import {addPortDialog} from './src/util/domAdd.js'
+
+/* Const */
+const d = document;
+const dialog = document.querySelector("#portafolio")
+
+/* Declare Events*/
+d.addEventListener("DOMContentLoaded",(e)=>{
+    dialog.showModal();
+});
+
+d.addEventListener("submit",(e)=>{
+
+})
