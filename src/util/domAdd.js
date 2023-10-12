9b301b5392f689cd0fbb62b70efcf3528f9f58a0
johanp0308 <111256873+johanp0308@users.noreply.github.com>
2023-10-12 00:26:49 -0500

    feat: :sparkles: Methods to add form

--- src/util/domAdd.js
+++ src/util/domAdd.js
@@ -13,8 +13,19 @@ const capitalize = (word) => {
 }
 
 
+export const getListCheck = (elements) =>{
+  let list = [];
+  elements.forEach(ele => {
+    if(ele.checked){
+      list.push(ele.value);
+    }
+  });
+  return list;
+}
+
+
 export const addPortDialog = async (dialog) => {
-  dialog.textContent = "";
+  dialog.innerhtml = "";
   let data = await getRelationShipsUser(1);
   let {
     name,
@@ -27,8 +38,9 @@ export const addPortDialog = async (dialog) => {
     languagesId,
     statusId,
   } = data;
-};
 
+  dialog()
+};
 
 export const loadedForm = async () => {
   let statuseshtml;
@@ -50,7 +62,7 @@ export const loadedForm = async () => {
   `
   habilitiesHtml = `
   <div class="content">
-    <h3>Habilities</h3>
+    <h5>Habilities</h5>
     ${allhabili.map( (e) =>{
       return `
       <div class="form-check">
@@ -64,7 +76,7 @@ export const loadedForm = async () => {
 
   languageshtml = `
   <div class="content">
-    <h3>Languages you Speak</h3>
+    <h5>Languages you Speak</h5>
     ${allLanguages.map( (e) =>{
       return `
       <div class="form-check">
@@ -78,7 +90,7 @@ export const loadedForm = async () => {
   
   skillsHtml = `
   <div class="content">
-    <h3>Progamming Languages</h3>
+    <h5>Progamming Languages</h5>
     ${allskills.map( (e) =>{
       return `
       <div class="form-check">
@@ -90,13 +102,13 @@ export const loadedForm = async () => {
     }).join("")}
   </div>`
 
-  let form = `
+  let form =/* html*/ `
   <div class="head">
 						<h3>Form</h3>
 					</div>
 					<!-- conteido --></i>
 					<h1>Form of Profile</h1>
-					<form action="" class="flex">
+					<form action="" class="form-profile-add" id="form-profile">
 						<div class="mb-3">
 							<label for="basic-url" class="form-label">Name</label>
 							<div class="input-group">
@@ -108,7 +120,7 @@ export const loadedForm = async () => {
 						<div class="mb-3">
 							<label for="basic-url" class="form-label">Your image URL</label>
 							<div class="input-group">
-								<span class="input-group-text" id="basic-addon3">https://example.com/users/img.jpg</span>
+								<span class="input-group-text" id="basic-addon3">https://example.com/img.jpg</span>
 								<input type="url" class="form-control" required id="basic-url" aria-describedby="basic-addon3 basic-addon4" name="img">
 							</div>
 							<div class="form-text" id="basic-addon4">Here you must put the url where your photo is on the web - required</div>
@@ -126,7 +138,7 @@ export const loadedForm = async () => {
 							<label for="basic-url" class="form-label">Phone</label>
 							<div class="input-group">
 								<span class="input-group-text" id="basic-addon3">+57</span>
-							  <input type="number" class="form-control" name="phone">
+							  <input type="number" required class="form-control" name="phone">
 							</div>
 							<div class="form-text" id="basic-addon4"></div>
 						  </div>
@@ -134,23 +146,93 @@ export const loadedForm = async () => {
 							<label for="basic-url" class="form-label">Gihub UserName</label>
 							<div class="input-group">
 								<span class="input-group-text" id="basic-addon3">@</span>
-							  <input type="text" class="form-control" name="github">
+							  <input type="text" required class="form-control" name="github">
 							</div>
 							<div class="form-text" id="basic-addon4"></div>
 						  </div>
 						<div class="mb-3">
 							<label for="basic-url" class="form-label">Linkedin</label>
 							<div class="input-group">
-							  <input type="text" class="form-control" name="linkedin">
+							  <input type="text" required class="form-control" name="linkedin">
 							</div>
 							<div class="form-text" id="basic-addon4"></div>
 						  </div>
-            <h3>Status</h3>
+            <div class="input-group">
+              <span class="input-group-text">Description</span>
+              <textarea class="form-control" required aria-label="Description" name="aboutme"></textarea>
+            </div>
+            <h5>Status</h5>
             ${statuseshtml}
             ${habilitiesHtml}
             ${languageshtml}
             ${skillsHtml}
-
+            <div class="experences-form">
+            <h3>Experiencs</h3>
+            <div class="mb-3">
+              <label for="basic-url" class="form-label"><b>Experence 1</b></label>
+              <div class="input-group">
+              <span class="input-group-text" id="basic-addon3">Company</span>
+                <input type="text" class="form-control" name="company1">
+              <span class="input-group-text" id="basic-addon3">Position</span>
+                <input type="text" class="form-control" name="position1">
+              </div>
+              <div class="input-group mb-3">
+                <select class="form-select" id="inputGroupSelect02" name="duration1">
+                  <option value="menos de 1 año">Menos de 1 año</option>
+                  <option value="mayor a 1 año y menor a 5">Mayor a 1 año y menor a 5</option>
+                  <option value="mayor a 5 años">Mayor a 5 años</option>
+                </select>
+                <label class="input-group-text" for="inputGroupSelect02">Duration</label>
+                </div>
+              <div class="input-group">
+                <span class="input-group-text">Description</span>
+                <textarea class="form-control" aria-label="With textarea" name="description1"></textarea>
+                </div>
+              <div class="form-text" id="basic-addon4"></div>
+            </div>
+            <div class="mb-3">
+              <label for="basic-url" class="form-label"><b>Experence 2</b></label>
+              <div class="input-group">
+              <span class="input-group-text" id="basic-addon3">Company</span>
+                <input type="text" class="form-control" name="company2">
+              <span class="input-group-text" id="basic-addon3">Position</span>
+                <input type="text" class="form-control" name="position2">
+              </div>
+              <div class="input-group mb-3">
+                <select class="form-select" id="inputGroupSelect02" name="duration2">
+                  <option value="menos de 1 año">Menos de 1 año</option>
+                  <option value="mayor a 1 año y menor a 5">Mayor a 1 año y menor a 5</option>
+                  <option value="mayor a 5 años">Mayor a 5 años</option>
+                </select>
+                <label class="input-group-text" for="inputGroupSelect02">Duration</label>
+                </div>
+              <div class="input-group">
+                <span class="input-group-text">Description</span>
+                <textarea class="form-control" aria-label="With textarea" name="description2"></textarea>
+                </div>
+              <div class="form-text" id="basic-addon4"></div>
+            </div><div class="mb-3">
+              <label for="basic-url" class="form-label"><b>Experence 3</b></label>
+              <div class="input-group">
+              <span class="input-group-text" id="basic-addon3">Company</span>
+                <input type="text" class="form-control" name="company3">
+              <span class="input-group-text" id="basic-addon3">Position</span>
+                <input type="text" class="form-control" name="position3">
+              </div>
+              <div class="input-group mb-3">
+                <select class="form-select" id="inputGroupSelect02" name="duration3">
+                  <option value="menos de 1 año">Menos de 1 año</option>
+                  <option value="mayor a 1 año y menor a 5">Mayor a 1 año y menor a 5</option>
+                  <option value="mayor a 5 años">Mayor a 5 años</option>
+                </select>
+                <label class="input-group-text" for="inputGroupSelect02">Duration</label>
+                </div>
+              <div class="input-group">
+                <span class="input-group-text">Description</span>
+                <textarea class="form-control" aria-label="With textarea" name="description3"></textarea>
+                </div>
+              <div class="form-text" id="basic-addon4"></div>
+            </div>
 						<button type="submit" class="btn btn-primary">Submit</button>
 					</form>
   
