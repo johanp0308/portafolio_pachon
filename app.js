e876530c18b8393e7b6b068e1f12dfc2c04bd893
johanp0308 <111256873+johanp0308@users.noreply.github.com>
2023-10-11 20:55:07 -0500

    feat: :construction: The event was added to close the modal, in addition to reloading the form, with the json data

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
 /* Imports */
-// import {addPortDialog} from './src/util/domAdd.js'
+import {addPortDialog, loadedForm} from './src/util/domAdd.js'
 
 /* Const */
 const d = document;
-const dialog = document.querySelector("#portafolio")
+const dialog = d.querySelector("#portafolio")
+const content = d.querySelector(".content-data");
+// console.log(content)
+
+const initForm = async () =>{
+    content.innerHTML = "";
+    console.log()
+    content.insertAdjacentHTML("beforeend", await loadedForm())
+}
+
+const initTable = () =>{
+    
+}
+
 
 /* Declare Events*/
 d.addEventListener("DOMContentLoaded",(e)=>{
-    dialog.showModal();
+    dialog.style.display = "none";
+    initForm();
 });
 
 d.addEventListener("submit",(e)=>{
-
+    e.preventDefault()
+    console.log(Object.fromEntries(new FormData(e.target)))
 })
+
+d.addEventListener("click",(e)=>{
+    if(e.target.matches("button#dialog-close.btn-close")){
+        console.log(e.target)
+        dialog.close();
+    }
+});
+
