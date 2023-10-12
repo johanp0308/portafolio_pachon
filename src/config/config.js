cef4b993736d2908252c992548730cb6a992d4e5
johanp0308 <111256873+johanp0308@users.noreply.github.com>
2023-10-11 20:57:07 -0500

    feat: :sparkles: Add an object with json endpoints

--- src/config/config.js
+++ src/config/config.js
@@ -2,5 +2,12 @@
 export const env = {
    ssl:"http://",
    host:"127.0.0.10",
-   port:"5010"
+   port:"5100"
+}
+
+export const endpoints ={
+   skills:"skills",
+   habilities:"habilities",
+   languages:"languages",
+   statuses:"statuses"
 }
